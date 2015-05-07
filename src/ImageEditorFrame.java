45fc8e4ff9ac156d4c5123618f9d549cfc68deff
DarkPhoeniz <l_zembrot@hotmail.com>
2015-05-07 11:19:08 +0200

    menubar

--- src/ImageEditorFrame.java
+++ src/ImageEditorFrame.java
@@ -11,4 +11,22 @@ public class ImageEditorFrame extends JFrame {
 		setSize(400,300);
 		setVisible(true);
 	}
+	
+	private void createMenuBar(){
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		JMenu menuFile = new JMenu("File");
+		menuBar.add(menuFile);
+		JMenuItem menuItemOpen = new JMenuItem("Open");
+		menuFile.add(menuItemOpen);
+		menuItemOpen.addActionListener( new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				onOpen();
+			}
+		});
+	}
+	
+	private void onOpen(){
+		JOptionPane.showMessageDialog(this, "Open Selected");
+	}
 }
